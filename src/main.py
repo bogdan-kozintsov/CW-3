8582c119c33981f894a3ba0f6b45fd3f5beab46b
bogdan.kozintsov <bogdan.kozintsov@gmail.com>
2024-02-16 14:59:09 +1100

    fix def formate_date fix def last_five_operations

--- src/main.py
+++ src/main.py
@@ -1,36 +1,51 @@
-# import json
-
-# from utils import open_file, sorted_date, format_operation
-
-# file = open_file() # Все операции
-# date = modify_date(file) # только executed
-# data = date.sort()[:5] # [start:stop:step] сортируем по дате, и выводим последние 5 операций
-#
-# for transaction in date:
-#     formated = format_operation(transaction)
-#     print(formated)
-
-
-# ниже код из консультации с наставником:
 
 
 
-from src.utils import operations_list, get_operations, get_executed_only, sort_by_date, last_five_operations, \
-    get_formated_operation
+from src.utils import operations_list, get_operations, get_executed_only, sort_by_date, last_five_operations, formate_date, hide_number
 
 all_operations = get_operations(operations_list)
-
 filtered_operations = get_executed_only(all_operations)
-# print(filtered_operations)
-# print(len(filtered_operations))
-
 sorted_operations = sort_by_date(filtered_operations)
-# print(sorted_operations)
-
 recent_operations = last_five_operations(sorted_operations)
-print(recent_operations)
 
-#
-#
-# for operation in five_operations:
-#     print(get_formated_operation(operation))
+# for s_ops in sorted_operations:
+#      print(s_ops['date'])
+
+# for r_ops in reversed(recent_operations):
+#      print(r_ops['date'])
+
+for operation in recent_operations:
+     date = formate_date(operation['date'])
+     description = operation['description']
+     # operation_from = operation['from']
+     operation_to = hide_number(operation['to'])
+     print(f'{date} {description}\n->{operation_to}')
+     # formated_operation = operation
+
+
+
+# "date" (форматированная) "description"
+# "from" (с шифром) -> "to" (с шифром)
+# "amount" руб.
+
+
+    # "id": 441945886,
+    # "state": "EXECUTED",
+    # "date": "2019-08-26T10:50:58.294041",
+    # "operationAmount": {
+    #   "amount": "31957.58",
+    #   "currency": {
+    #     "name": "руб.",
+    #     "code": "RUB"
+    #   }
+    # },
+    # "description": "Перевод организации",
+    # "from": "Maestro 1596837868705199",
+    # "to": "Счет 64686473678894779589"
+
+
+# # Пример вывода для одной операции:
+# 14.10.2018 Перевод организации
+# Visa Platinum 7000 79** **** 6361 -> Счет **9638
+# 82771.72 руб.
+
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import os
 import json
-from datetime import datetime
 from config import ROOT_DIR
 
 operations_list = os.path.join(ROOT_DIR, 'operations.json')
@@ -28,7 +27,7 @@ def sort_by_date(executed_operations):
 
 def last_five_operations(sorted_operations):
     """Функция принимает отсортированные выполненные операции и возвращает 5 последних операций"""
-    five_operations = sorted_operations[:5]
+    five_operations = reversed(sorted_operations[:5])
     return five_operations
 
 
@@ -36,7 +35,11 @@ def get_formated_operation():
     """Функция принимает последние 5 операций и возвращает их в отформатированном виде"""
 
 
+
+
+
 def hide_number(requisites: str):
+    """Функция шифрования номера счета или карты"""
     parts = requisites.split()
     number = parts[-1]
     if requisites.lower().startswith('счет'):
@@ -46,73 +49,15 @@ def hide_number(requisites: str):
     parts[-1] = hided_number
     return ' '.join(parts)
 
-print(hide_number("Maestro 1308795367077170"))
+
+
 
 
 def formate_date(date):
+    """Функция форматирования даты"""
     date_only = date[:10]
     parts = date_only.split('-')
     return '.'.join(reversed(parts))
 
-print(formate_date("2019-07-13T18:51:29.313309"))
-
-# def sorted_date(date):
-#     new_data = []
-#     for item in date:
-#         if item == {}:
-#             continue
-#         elif item['state'] == 'EXECUTED': # {} .get()
-#             new_data.append(item)
-#
-#     sort_date = sorted(new_data, key=lambda x: x['date'], reverse=True)
-#     five_transactions = sort_date[:5]
-#
-#     return five_transactions
 
 
-# def format_date(_date):
-#     date_format = _date.split('T'[0].split('-'))
-#     date_format = '{}.{}.{}'.format(date_format[2], date_format[1], date_format[0])
-#     return date_format
-#
-# print(format_date(open_file['date']))
-#
-# def silence_card(card):
-#     card_silence = '{} {}** **** {}'.format(card[:-12], card[-10:-8], card[-4:])
-#     return card_silence
-#
-# print(silence_card(sorted_date(open_file())))
-
-
-# def silence_account(account):
-#     account_silence = 'Счёт **{}'.format(account[-4:])
-#     return account_silence
-#
-# print(silence_account(sorted_date(open_file())))
-
-# def format_operation(transaction):
-#     """
-#     "id": 214024827,
-#     "state": "EXECUTED",
-#     "date": "2018-12-20T16:43:26.929246",
-#     "operationAmount": {
-#       "amount": "70946.18",
-#       "currency": {
-#         "name": "USD",
-#         "code": "USD"
-#       }
-#     },
-#     "description": "Перевод организации",
-#     "from": "Счет 10848359769870775355",
-#     "to": "Счет 21969751544412966366"
-#     """
-#
-#
-#     transaction['date'] = sorted_date(transaction['date'])
-#     # обработать если нет ключа from
-#     transaction['from'] = modify_bill(transaction['from'])
-#     #
-#     transaction['to'] = modify_bill(transaction['to'])
-#     # оставить строку для принта
-#
-#     return f"14.10.2018 Перевод организации\nVisa Platinum 7000 79** **** 6331 -> Счёт **9638\n82771.72 руб."
